4426b39fa2c16cf565b97c30ce26580595b1b75a
AmeliaDWho <71261272+AmeliaDWho@users.noreply.github.com>
2020-09-16 09:34:20 -0300

    Update Amelia.sql

--- src/main/sql/s20201/exercicios/exercicio1/Amelia.sql
+++ src/main/sql/s20201/exercicios/exercicio1/Amelia.sql
@@ -1,10 +1,9 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  Gabri
- * Created: 14 de set. de 2020
- */
+drop table if exists pessoa  cascade;
+create table pessoa(
+nome varchar,
+endereco varchar
+);
+insert into pessoa values ('Luiz Andre','end');
+insert into pessoa values ('Luiz','end2');
 
+select * from pessoa;
